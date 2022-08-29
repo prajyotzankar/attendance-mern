d3328426be5b935d5ab2ff489eadccf63ce6c376
prajyotzankar <zankarprajyotsushil@gmail.com>
2022-08-29 22:29:50 +0530

    Navbar Fixed

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,24 +6,24 @@ export default class NavBar extends Component {
   render() {
     return (
       // <div className="container">
-        <div className="nav">
-          <div className="logo">
-            <div className="logo-text">
-              <b>A</b>ttendance <b>A</b>pp
-            </div>
-          </div>
-          <div class="menu">
-            <Link to="/" className="menu-item student">
-              <b>S</b>tudents
-            </Link>
-            <Link to="/student/add" className="menu-item admitStudent">
-              <b>A</b>dmit <b>S</b>tudent
-            </Link>
-          </div>
-          <div className="logoutButton">
-            <div className="logoutButton-text">Log Out</div>
+      <div className="nav">
+        <div className="logo">
+          <div className="logo-text">
+            <b>A</b>ttendance <b>A</b>pp
           </div>
         </div>
+        <div className="menu">
+          <Link to="/" className="menu-item student">
+            <b>S</b>tudents
+          </Link>
+          <Link to="/student/add" className="menu-item admitStudent">
+            <b>A</b>dmit <b>S</b>tudent
+          </Link>
+        </div>
+        <div className="logoutButton">
+          <div className="logoutButton-text">Log Out</div>
+        </div>
+      </div>
       // </div>
     );
   }
